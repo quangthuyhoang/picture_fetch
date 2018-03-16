38ab1ac8c11fde47ab567a45611544f101fcef1f
quangthuyhoang <qtnhoang@gmail.com>
2018-03-15 22:19:42 -0500

    Updates

---
+++ client.RandomPos.js
@@ -0,0 +1,10 @@
+'use strict';
+var picList = document.getElementsByClassName('PictureBox');
+
+(function showPic(){
+	picList.forEach(funnction(pic){
+		pic.style.display = "block"
+		pic.style
+	})
+
+})();
